f131fc9850ceae41a45ab7df67d82c6d74fd0010
ChitranshBP <chitransh@brandingpioneers.co.in>
2026-06-10 14:05:06 +0530

    all work done 5

--- index.php
+++ index.php
@@ -526,7 +526,7 @@ require __DIR__ . '/includes/header.php';
                 <div id="reviewTrack" class="flex snap-x snap-mandatory overflow-x-auto scroll-smooth gap-6 pb-2 -mx-2 px-2 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
                     <?php foreach ($testimonials as [$name, $role, $place, $quote, $border]): ?>
                         <div class="snap-start shrink-0 w-full sm:w-[calc((100%-1.5rem)/2)] lg:w-[calc((100%-3rem)/3)]">
-                            <div class="relative bg-white rounded-lg shadow-sm hover:shadow-md transition overflow-hidden border-t-4 <?= $border ?> h-full">
+                            <div class="relative bg-white rounded-lg  shadow-sm hover:shadow-md transition overflow-hidden border-t-4 <?= $border ?> h-full">
                                 <svg class="absolute top-4 right-4 w-16 h-16 text-slate-100" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="M9.983 3v7.391c0 5.704-3.731 9.57-8.983 10.609l-.995-2.151c2.432-.917 3.995-3.638 3.995-5.849h-4v-10h9.983zm14.017 0v7.391c0 5.704-3.748 9.571-9 10.609l-.996-2.151c2.433-.917 3.996-3.638 3.996-5.849h-3.983v-10h9.983z"/>
                                 </svg>
