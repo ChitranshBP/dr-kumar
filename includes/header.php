8b56609d21ac5ecfe48e23b3a932956384960e3b
anshrealestate <anshrealestate@gmail.com>
2026-06-23 18:38:00 +0530

    Add Google Search Console verification meta tag

--- includes/header.php
+++ includes/header.php
@@ -174,6 +174,7 @@ if (!isset($schema_about)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="google-site-verification" content="kfxI6o8pHnk07vxh6FQc48BQ-xZt6jCY1Dj2nC2MynY" />
 
     <title><?= htmlspecialchars($page_title) ?></title>
     <meta name="description" content="<?= htmlspecialchars($page_description) ?>">
